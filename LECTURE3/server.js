bcc5fdeada3a09f53657709e77ebcbb606bcf319
prachiaswal <prachiaswal15@gmail.com>
2026-08-22 17:03:46 +0530

    Initial commit

--- LECTURE3/server.js
+++ LECTURE3/server.js
@@ -1,56 +1,50 @@
 const http = require("http");
-const users = [
-    { id: 101, name: "alex", email: "GZD1C@example.com" },
-    { id: 102, name: "Ravi", email: "ravi@com" },
-    { id: 103, name: "alex", email: "vasu@com" },
+
+const user=[
+    {id:101,name:"siya",email:"siya@gmail.com"},
+    {id:102,name:"riya",email:"riya@gmail.com"},
+    {id:103,name:"priya",email:"priya@gmail.com"},
+    {id:104,name:"diya",email:"diya@gmail.com"},
 ]
 
+const server =  http.createServer((req,res)=>{
+   // console.log(req.url);
+   // console.log(req.headers);
 
-const server = http.createServer((req, res) => {
-    // console.log(req.url);
-    // console.log(req.headers);
-  
-    if (req.url == "/" && req.method == "GET") {
-        res.writeHead(200, { "content-type": "text/html" });
-        res.write("<h1>welcome to server<h1>");
-        res.end();
-    } else if (req.url == "/about" && req.method == "GET") {
-        res.writeHead(200, { "content-type": "text/html" });
-        res.write("<h1>welcome to about page<h1>");
-        res.end();
-    } else if (req.url == "/contact" && req.method == "GET") {
-        res.writeHead(200, { "content-type": "text/html" });
-        res.write("<h1>welcome to contactpage<h1>");
-        res.end();
-
-    } else if (req.url == "/users" && req.method == "GET") {
-        res.writeHead(200, { "content-type": "application/json" });
-        res.write(JSON.stringify(users));
-        res.end();
-    } else if (req.url == "/users" && req.method == "POST") {
-        let body = "";
-        req.on("data", (chunk) => {
-            body += chunk;
-        });
-        req.on("end", () => {
-            const user = JSON.parse(body);
-            users.push(user);
-            res.writeHead(201, { "content-type": "application/json" });
-            res.write(JSON.stringify({"success":true,"message":"user created successfully"
-                
-            }));
-            res.end();
-        });
-
-
-    }
-    else {
-    res.writeHead(200, { "Content-type": "text/html" });
+if(req.url=="/"&& req.method=="GET"){
+    res.writeHead(200,{"content-type":"text/html"});
+    res.write("<h1>welcome to server<h1>");
+    res.end();
+}else if(req.url=="/about"&& req.method=="GET"){
+    res.writeHead(200,{"content-type":"text/html"});
+    res.write("<h1>welcome to about page<h1>");
+    res.end();
+}else if(req.url=="/contact"&& req.method=="GET"){
+    res.writeHead(200,{"content-type":"text/html"});
+    res.write("<h1>welcome to contactpage<h1>");
+    res.end();
+}else if(req.url=="/user"&& req.method=="GET"){
+    res.writeHead(200,{"content-type":"application/json"});
+    res.write(JSON.stringify(user));
+    res.end();
+}else if(req.url=="/user"&& req.method=="POST"){
+    let body=""
+    req.on("data",(chunk)=>{
+        body+=chunk;
+    });
+    req.on("end",()=>{
+        const users = JSON.parse(body);
+        user.push(users);
+        res.writeHead(200,{"content-type":"application/json"});
+        res.write(JSON.stringify({"success":true,"message":"users created successfully"}));
+        res.end();                    
+    })    
+}else{
+    res.writeHead(404,{"Content-type":"text/html"});
     res.write("<h1>404 page notfound<h1>");
     res.write("page not found");
-}
-res.end()     
-});
+}   
+})
 server.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+    console.log("Server running on http://localhost:3000"); 
 });
--- LECTURE5/server.js
+++ LECTURE5/server.js
@@ -4,11 +4,11 @@ const app=express();
 app.use(express.json());
 
 const employees=[
-    {employeeId:1,employeeName:"John",department:"IT" ,salary:10000},    
-    {employeeId:2,employeeName:"Jane",department:"HR" ,salary:12000},
-    {employeeId:3,employeeName:"Bob",department:"Finance" ,salary:15000},
-    {employeeId:4,employeeName:"Alice",department:"IT" ,salary:11000},
-    {employeeId:5,employeeName:"Charlie",department:"HR" ,salary:13000},
+    {employeeId:1,employeeName:"prachi",department:"IT" ,salary:10000},    
+    {employeeId:2,employeeName:"itika",department:"HR" ,salary:12000},
+    {employeeId:3,employeeName:"namita",department:"Finance" ,salary:15000},
+    {employeeId:4,employeeName:"anmol",department:"IT" ,salary:11000},
+    {employeeId:5,employeeName:"Tanisha",department:"HR" ,salary:13000},
 
 ]
 // read operation
