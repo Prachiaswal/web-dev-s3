4b655865f0305e12b0527e27d8cdd7e5d72ff512
prachiaswal <prachiaswal15@gmail.com>
2026-09-20 18:46:26 +0530

    Add LECTURE7 MVC Project

---
+++ LECTURE7/data/employeedata.js
@@ -0,0 +1,9 @@
+const employees=[
+    {employeeId:1,employeeName:"prachi",department:"IT" ,salary:10000},    
+    {employeeId:2,employeeName:"itika",department:"HR" ,salary:12000},
+    {employeeId:3,employeeName:"namita",department:"Finance" ,salary:15000},
+    {employeeId:4,employeeName:"anmol",department:"IT" ,salary:11000},
+    {employeeId:5,employeeName:"Tanisha",department:"HR" ,salary:13000},
+
+]
+ module.exports=employees;
